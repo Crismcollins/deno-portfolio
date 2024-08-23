7627fa58fb4852ddbe3c8083373cb5e82d092a47
crismcollins <cristobal.molina@meydey.com>
2024-08-23 15:54:24 -0400

    refresh token endpoints

--- helpers.ts
+++ helpers.ts
@@ -1,4 +1,3 @@
-import { GoogleOAuth } from "https://deno.land/x/google@0.0.8/oauth.ts";
 // import { serve } from "https://deno.land/std/http/server.ts";
 
 // export async function readData() {
@@ -13,17 +12,3 @@ import { GoogleOAuth } from "https://deno.land/x/google@0.0.8/oauth.ts";
 // export async function writeData(data: any) {
 //   await Deno.writeTextFile(DATA_FILE, JSON.stringify(data, null, 2));
 // }
-
-
-export function  GetLinkAuthorizeApp() {
-  const oauth2Client = new GoogleOAuth({
-    client_id: Deno.env.get("CLIENT_ID") ?? '',
-    client_secret: Deno.env.get("CLIENT_SECRET") ?? '',
-    redirect_uri: Deno.env.get("REDIRECT_URI") ?? '',
-    "scopes": ["https://www.googleapis.com/auth/drive"],
-  });
-  
-  const link = oauth2Client.buildAuthLink();
-  return link;
-}
-
