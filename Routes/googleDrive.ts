6ed714af739b21bc244be3c55b4d9ab9ee7f7c04
crismcollins <cristobal.molina@meydey.com>
2024-09-12 03:35:39 -0300

    fix token refresh

--- Routes/googleDrive.ts
+++ Routes/googleDrive.ts
@@ -1,14 +1,15 @@
 import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-import { getFileByName, getGoogleDriveStorage } from "../GoogleDrive/fetches.ts";
+import { getFileByName, getGoogleDriveStorage, refreshToken } from "../GoogleDrive/fetches.ts";
+import { getGoogleDriveToken } from "../GlobalStates/tokenState.ts";
 
 export function GoogleDriveRoutes(app: Hono) {
 
-  app.get("/storage", async (c) => {
+  app.get("/storage/all", async (c) => {
     const data = await getGoogleDriveStorage();
     return c.json(data.files);
   });
 
-  app.get("/profile-picture", async (c) => {
+  app.get("/storage/profile-picture", async (c) => {
     const imageName = "cris";
     const data = await getFileByName(imageName, ['image/jpeg', 'image/png']);
     const imageLink = `https://drive.google.com/thumbnail?id=${data.id}`;
--- main.ts
+++ main.ts
@@ -3,15 +3,15 @@ import {
   FilesRoutes,
   TokenRoutes
 } from "./Routes/index.ts";
-import getResumeHTML from "./CV/index.ts";
 import { getGoogleDriveToken, setAppHono } from "./GlobalStates/tokenState.ts";
 import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
 import { cors } from "https://deno.land/x/hono@v4.3.11/middleware.ts";
-import { refreshToken } from "./GoogleDrive/fetches.ts";
 import { UploadModels } from "./db/index.ts";
 import { ManagerRoutes } from "./Routes/manager.ts";
 import { ClientRoutes } from "./Routes/client.ts";
 import { redirectUrl } from "./Routes/middlewares.ts";
+import { refreshToken } from "./GoogleDrive/fetches.ts";
+import getResumeHTML from "./CV/index.ts";
 
 const app = new Hono();
 setAppHono(app);
@@ -29,11 +29,10 @@ app.use('*', redirectUrl);
 
 app.get("/", async (c) => {
   const token = getGoogleDriveToken();
-
+  console.log(token)
   if (!token) {
     await refreshToken();
   }
-
   const resumeHtml = await getResumeHTML("es");
   return c.html(resumeHtml);
 });
